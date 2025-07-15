6b551cd54b37754198e8446c8c96421de9eb1445
MariyaPD-07 <mariyapd07@gmail.com>
2025-07-15 23:04:22 +0530

    second commit

--- src/components/StudentNavbar.jsx
+++ src/components/StudentNavbar.jsx
@@ -12,7 +12,7 @@ const StudentNavbar = () => {
 
 <nav class="navbar navbar-expand-lg bg-warning">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Student App</a>
+    <a class="navbar-brand" href="#">Student API</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
